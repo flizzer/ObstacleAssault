9ed50135b84420311ac92b6de4c7b4218b91150d
Brian <briandavis1977@gmail.com>
2022-09-25 16:33:27 -0400

    updating MyVector Y position to make the platform move

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,9 +15,6 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-
-	SetActorLocation(MyVector);
-
 }
 
 // Called every frame
@@ -25,5 +22,7 @@ void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	MyVector.Y = MyVector.Y + 1;
+	SetActorLocation(MyVector);
 }
 
