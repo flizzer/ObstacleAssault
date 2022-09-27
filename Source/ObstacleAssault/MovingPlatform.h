1ffd271b4e466fa401fadd7b764c04b23e8c63fb
Brian <briandavis1977@gmail.com>
2022-09-26 22:21:27 -0400

    adding readonly prop and computing distance traveled of moving platform

--- Source/ObstacleAssault/MovingPlatform.h
+++ Source/ObstacleAssault/MovingPlatform.h
@@ -25,4 +25,9 @@ public:
 
 	UPROPERTY(EditAnywhere, Category="Moving Platform")
 	FVector PlatformVelocity = FVector(100,0,0);
+
+	FVector StartLocation;
+
+	UPROPERTY(VisibleAnywhere)
+	float DistanceFromStart = -1;
 };
